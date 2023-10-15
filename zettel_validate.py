00a64708dd81eb999e920a7aa44020fa1c377001
flengyel <florian.lengyel@gmail.com>
2023-10-15 18:47:28 -0400

    Capture the all issues in one go.

--- zettel_validate.py
+++ zettel_validate.py
@@ -14,40 +14,40 @@ validation_stats = {
 def zettel_validate(text, filename_without_extension=''):
     global validation_stats
     
-    is_valid = True
+    issues=[]
     
     # Extract YAML header
     yaml_header_match = re.search(r'---\n(.*?)\n---', text, re.DOTALL)
     if not yaml_header_match:
-        print(filename_without_extension, ": Invalid or missing YAML header")
+        issues.append(f"{filename_without_extension}: Invalid or missing YAML header")
         validation_stats['invalid_yaml_header'] += 1
+    try:
+        yaml_header = yaml_header_match.group(1)
+    except  AttributeError:
+        print('\n'.join(issues))
         return False
-    
-    yaml_header = yaml_header_match.group(1)
     
     try:
         header_dict = yaml.safe_load(yaml_header)
     except yaml.YAMLError:
-        print(filename_without_extension, ": YAML header parsing exception")
+        issues.append(f"{filename_without_extension}: YAML header parsing exception")
         validation_stats['invalid_yaml_header'] += 1
+        print('\n'.join(issues))
         return False
         
     # Validate YAML header fields
     if 'title' not in header_dict or 'reference-section-title' not in header_dict:
-        print(filename_without_extension, ": YAML header missing title: or reference-section-title:")
+        issues.append(f"{filename_without_extension}: YAML header missing title: or reference-section-title:")
         validation_stats['invalid_yaml_header'] += 1
-        is_valid = False
-    
+        
     title = header_dict.get('title', '')
     ref_section_title = header_dict.get('reference-section-title', '')
-   
-    
+       
     # Validate title
     title_match = re.fullmatch(r'((\w{1,4}\.){2,}\d\w{3}) (.+)', title)
     if not title_match:
-        print(filename_without_extension, ": Invalid title format")
+        issues.append(f"{filename_without_extension}: Invalid title format")
         validation_stats['invalid_title_format'] += 1
-        is_valid = False
     else:
         captured_id, _, captured_title = title_match.groups()
         
@@ -55,36 +55,34 @@ def zettel_validate(text, filename_without_extension=''):
         if filename_without_extension:
             if filename_without_extension != captured_id:
                 # Add a new validation error type for filename mismatch
-                print(filename_without_extension, ": Filename ID mismatch")
+                issues.append(f"{filename_without_extension}: Filename ID mismatch")
                 validation_stats['filename_id_mismatch'] += 1
-                is_valid = False
-                
-
+           
     # Extract content after YAML header
     content = text[yaml_header_match.end():].strip()
     
     # Validate H1 header
     h1_header_match = re.match(r'# ' + re.escape(title) + r'\n', content)
     if not h1_header_match:
-        print(filename_without_extension, ": H1 header YAML ID mismatch")
+        issues.append(f"{filename_without_extension}: H1 header YAML ID mismatch")
         validation_stats['h1_mismatch'] += 1
-        is_valid = False
     
     # Extract and validate wikilinks
     wikilinks = re.findall(r'\[\[(.*?)\]\]', content)
     if len(wikilinks) == 0:
-        print(filename_without_extension, ": Missing wikilinks")
+        issues.append(f"{filename_without_extension}: Missing wikilinks")
         validation_stats['missing_wikilinks'] += 1
-        is_valid = False
     
     # Extract and validate hashtags
     hashtags = re.findall(r' (#[\w]+)', content)
     if len(hashtags) == 0:
-        print(filename_without_extension, ": Missing or improperly indented hashtags")
+        issues.append(f"{filename_without_extension}: Missing or improperly indented hashtags")
         validation_stats['missing_hashtags'] += 1
-        is_valid = False
     
-    return is_valid
+    if len(issues) > 0:
+        print('\n'.join(issues))
+        return False
+    return True
 
 if __name__=="__main__":
     sample_text = '''---
